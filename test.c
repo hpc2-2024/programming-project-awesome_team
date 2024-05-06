71f8f7571140b35e73fa5709f4c58f61b67dedb4
aleph <nujabes.feather.xor@gmail.com>
2024-05-06 12:23:24 +0200

    initial commit

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <math.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+    printf("Test.\n");
+    int i = 5; 
+    float x = M_PI;
+    printf("Done.\n");
+    return 0;
+}
