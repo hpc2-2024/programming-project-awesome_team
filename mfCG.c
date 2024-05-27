c2d95a45f8eed12c9e63be13f9b02e42a5d5640d
timoh <thauser@DESKTOP-JQTJ275>
2024-05-27 16:17:15 +0200

    corrections and additions from later exercises (iteration count, random init x)

--- mfCG.c
+++ mfCG.c
@@ -9,6 +9,11 @@ double fun(double x, double y){
     return sin(y*M_PI)*sin(x*M_PI)*2.0*M_PI*M_PI;
 }
 
+double fun_solution(double x, double y){
+    return sin(x*M_PI)*sin(y*M_PI);
+}
+
+
 /*! Calculating the dot (scalar) product of 2 vectors */
 double dot(double v[], double w[], int size) {
     double sum = 0;
@@ -27,7 +32,7 @@ void mfMult(int N, double r[], double y[], double h){
     for (int i=1;i<N+1;i++){
         for (int j=1;j<N+1;j++){
             y[(N+2)*i+j]=4*r[(N+2)*i+j]-r[(N+2)*(i+1)+j] -r[(N+2)*i+j-1]-r[(N+2)*(i-1)+j]-r[(N+2)*i+j+1];
-            y[(N+2)*i+j]=h*h*y[(N+2)*i+j];
+            y[(N+2)*i+j]=(1/(h*h))*y[(N+2)*i+j];
         }
     }
 }
@@ -48,9 +53,9 @@ void vec_print(int N, double vec[], char name[]){
 
 int main(int argc, char** argv){
     // initilize variables
-    int N = 6; // N^2 is the number of inner points in our lattice
+    int N = 30; // N^2 is the number of inner points in our lattice
     int N2 = (N+2)*(N+2);
-    double epsilon = 0.5;
+    double epsilon = 0.0000001;
     double h = 1.0/(N+1);
     // init x0
     double alpha = 0;
@@ -74,10 +79,16 @@ int main(int argc, char** argv){
         }
 
     }
+
+    int seed = 123456;
     // inner points of x_0,b
     for (int i = 1;i<N+1;i++) {
         for (int j = 1;j<N+1;j++){
-            x[(N+2)*i+j]=1;
+            // randomly initialize x with values in (0,1)
+            srand(seed + i);
+            double r = (double)rand() / (double)RAND_MAX;
+            x[(N+2)*i+j]=r;
+
             b[(N+2)*i+j]=fun(i*h,j*h);
         }
 
@@ -91,9 +102,12 @@ int main(int argc, char** argv){
         p[i]=r[i]*(-1);
     }
     err0 = sqrt(dot(r,r,N2));
-
+    
+    int number_of_iterations = 0;
     do
     {
+        number_of_iterations++;
+
         mfMult(N,p,m,h);
         double dot_rk_rk = dot(r,r,N2);
         alpha=dot_rk_rk/dot(p,m,N2);
@@ -113,9 +127,19 @@ int main(int argc, char** argv){
 
     } while (sqrt(dot(r,r,N2))/err0 >= epsilon);
 
-    vec_print(N,x,"vector x");
+    // Compute the relative absolute difference 
+    double abs_diff = 0.0;
+    double abs_sol = 0.0;
+    for (int i = 1;i<N+1;i++) {
+        for (int j = 1;j<N+1;j++){
+            double x_sol = fun_solution(i*h,j*h);
+            abs_diff += fabs(x[(N+2)*i+j] - x_sol);
+            abs_sol += x_sol;
+        }
+    }
+    printf("Run %d iterations.\n",number_of_iterations);
+    printf("Relative absolute difference between exact and approx. solution: %f%%\n", 100.0 * abs_diff / abs_sol);
 
-    printf("%f \n", sqrt(dot(r,r,N2)));
 
     // free allocated memory
     free(x);
