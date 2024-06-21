12cf0ab91efd532059f1b8509017bc8895050107
aleph <nujabes.feather.xor@gmail.com>
2024-06-21 20:17:45 +0200

    delete unnecessary files

--- temp.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-n = 5 
-
-A = np.diag(4 * np.ones(n**2)) + np.diag(-1 * np.ones(n**2-1),1) + np.diag(-1 * np.ones(n**2-1),-1) + np.diag(-1 * np.ones(n**2-n),n) + np.diag(-1 * np.ones(n**2-n),-n)  #+ np.diag(4 * np.ones(n**2))
