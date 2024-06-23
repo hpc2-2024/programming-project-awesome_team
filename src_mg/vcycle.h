9b15efe59e6fb4e2d32f810907d00c41bcc4b2d6
timoh <thauser@DESKTOP-JQTJ275>
2024-06-24 01:55:43 +0200

    w_cycle function placeholder

--- src_mg/vcycle.h
+++ src_mg/vcycle.h
@@ -95,6 +95,74 @@ void v_cycle(double** u, double **f, int N_start, int levels, int v, int dim, in
     }
 }
 
+void w_cycle(double** u, double** f, int N_start, int levels, int v, int dim, int use_stecil9, int debug) {
+    int vec_size;
+    int N = N_start;
+
+    for (int l = levels-1; l>=1; l--){
+        vec_size = get_vec_size(N,dim,1);
+        int N_coarser = dim_coarser(N); 
+        int vec_size_coarser = get_vec_size(N_coarser,dim,1);
+
+        double *r;
+        r = malloc(vec_size * sizeof(*r));
+        null_vec(r,vec_size);
+
+        // Apply Smoothing
+        smooth(u[l], f[l], N, v, dim, use_stencil9);
+        if (debug==1){
+            printf("u_%d after smoothing:\n",l);
+            vec_print(N,u[l],"u");
+        }
+
+        // Compute Residual
+        poisson_mat_vek(dim,N,u[l],r, use_stencil9);
+        axpy(r, -1, r, f[l], vec_size);
+
+        // Apply Restriction
+        restriction_half(r, N, f[l-1], N_coarser,dim);
+        if (debug==1){
+            printf("f_%d after smoothing:\n",l-1);
+            vec_print(N_coarser,f[l-1],"f");
+        }
+
+        // print_matrix(N_coarser + 2, f[l-1]);
+
+        null_vec(u[l-1], vec_size_coarser);
+
+        N = N_coarser;
+        free(r);
+    }
+
+    exact_solve(u[0],f[0],N,dim, use_stencil9);
+
+    for (int l=1;l<levels;l++){
+        vec_size = get_vec_size(N,dim,1);
+        int N_finer = dim_finer(N);
+        int vec_size_finer = get_vec_size(N_finer,dim,1);
+        
+        // init temporary vector  
+        double *u_temp;
+        u_temp = malloc(vec_size_finer * sizeof(*u_temp));
+        null_vec(u_temp, vec_size_finer);
+
+        // print_matrix(N+2, u[l-1]);
+
+        // Apply Prolongation
+        prolongation_simple(u[l-1], N, u_temp, N_finer,dim);
+
+        // correction
+        axpy(u[l], 1, u[l], u_temp, vec_size_finer);
+
+        // Smoothing
+        smooth(u[l], f[l], N_finer, v, dim, use_stencil9);
+
+        N = N_finer;
+        free(u_temp);
+    }
+
+}
+
 
 void f_cycle(double **u, double **f, int N_start, int levels, int v, int dim, int use_stencil9, int debug) {
     int N_coarsest = N_start;
