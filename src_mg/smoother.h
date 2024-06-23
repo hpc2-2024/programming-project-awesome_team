2f0200d3c79cc892e5a9235f7b40760cdb5e80d3
aleph <nujabes.feather.xor@gmail.com>
2024-06-23 13:11:20 +0200

    implemented richardson smoother

--- src_mg/smoother.h
+++ src_mg/smoother.h
@@ -28,6 +28,7 @@ void smooth_jacobi(double u[], double f[], int N, int v, int dim, int use_stenci
     int vec_size = get_vec_size(N,dim,1);
     double *u_new = (double *)malloc(vec_size * sizeof(double));
     null_vec(u_new,vec_size);
+    double omega = 0.6/4*h*h;
 
     // Jacobi iteration
     for (k = 0; k < v; k++) {
@@ -42,9 +43,9 @@ void smooth_jacobi(double u[], double f[], int N, int v, int dim, int use_stenci
 
             }
             else {
-                poisson_mat_vek(dim,N,u,u_new, 0);
-                axpy(u_new,-1,u_new,f,vec_size);
-                axpy(u_new,0.6/4*h*h,u_new,u,vec_size);
+                poisson_mat_vek(dim, N, u, u_new, 0); // u_new = A u
+                axpy(u_new, -1, u_new, f, vec_size); // u_new = f - A u
+                axpy(u_new, omega, u_new, u, vec_size); // u_new = u + omega * (f - A u)
             }
 
         } else if (dim==1) {
@@ -58,6 +59,47 @@ void smooth_jacobi(double u[], double f[], int N, int v, int dim, int use_stenci
         memcpy(u, u_new, vec_size * sizeof(double));
     }
 
+    free(u_new);
+}
+
+/**
+ * @brief Performs Richardson smoothing on the input vector `u`.
+ *
+ * Quite similar to the Jacobi method, except that we change parameter by which we weigh the update in each iteration.
+ *
+ * @param u Input vector to be smoothed.
+ * @param f Right-hand side vector.
+ * @param N Number of internal grid points.
+ * @param v Number of smoothing iterations.
+ * @param dim Dimension of the problem (1 or 2).
+ * @param use_stencil9  Flag for enabling 9 point stencil in the 2d case (0 or 1).
+ *
+ * @note Vectors `u` and `f` should be pre-allocated to the appropriate sizes.
+ */
+void smooth_richardson(double u[], double f[], int N, int v, int dim, int use_stencil9) {
+    int i, j, k;
+    double h = 1.0/(N+1);
+    double h2 = h * h;
+    int N_with_ghosts = N + 2;
+    int vec_size = get_vec_size(N,dim,1);
+    double *u_new = (double *)malloc(vec_size * sizeof(double));
+    null_vec(u_new,vec_size);
+
+    double omega = 0.3 / 4 * h * h; // algorithm is pretty sensitive to the weight
+    
+    // Modified Richardson Iteration (very similar to Jacobi)
+    // x_{k+1} = x_k + omega * (f - A x_k)
+    for (k = 0; k < v; k++) {
+        // Copy u to u_new (needed for the Jacobi update)
+        memcpy(u_new, u, vec_size * sizeof(double));
+
+        poisson_mat_vek(dim, N, u, u_new, 0); // u_new = A u 
+        axpy(u_new, -1.0, u_new, f, vec_size); // u_new = f - A u  
+        axpy(u_new, omega, u_new, u, vec_size); // u_new = u - omega * (A u + f)
+
+        // Swap u and u_new for the next iteration
+        memcpy(u, u_new, vec_size * sizeof(*u));
+    }
     free(u_new);
 }
 
@@ -76,6 +118,7 @@ void smooth_jacobi(double u[], double f[], int N, int v, int dim, int use_stenci
  */
 void smooth(double u[], double f[], int N, int v,int dim, int use_stencil9) {
     smooth_jacobi(u, f, N, v, dim, use_stencil9);
+    // smooth_richardson(u, f, N, v, dim, use_stencil9);
 }
 
 #endif
--- test.c
+++ test.c
@@ -3,7 +3,6 @@
 #include "utils.h"
 #include "mg_solver.h"
 
-
 /*! The function f of the exercise sheet*/
 double fun(double x, double y){
     return sin(y*M_PI)*sin(x*M_PI)*2.0*M_PI*M_PI;
@@ -22,128 +21,128 @@ void init_b(double b[],int N){
         }
     }
 }
-void test_norm(){
-    printf("Testing norm funtion - START \n");
-    // TC1
-    double x[4] = {1,1,1,1};
-    if (norm(x,4)==2) {
-        printf("TC 1 successful\n");
-    } 
+// void test_norm(){
+//     printf("Testing norm funtion - START \n");
+//     // TC1
+//     double x[4] = {1,1,1,1};
+//     if (norm(x,4)==2) {
+//         printf("TC 1 successful\n");
+//     } 
 
-    printf("Test norm function END\n\n");
-}
+//     printf("Test norm function END\n\n");
+// }
 
-void test_prolongation(){
-    printf("Testing  prolongation - START \n");
-    double u_2h[] = {   0,0,0,
-                        0,4,0,
-                        0,0,0   };
-    int N_2h=1;
+// void test_prolongation(){
+//     printf("Testing  prolongation - START \n");
+//     double u_2h[] = {   0,0,0,
+//                         0,4,0,
+//                         0,0,0   };
+//     int N_2h=1;
 
-    double u_h[5*5];
-    null_vec(u_h,25);
+//     double u_h[5*5];
+//     null_vec(u_h,25);
 
-    int N_h = 3;
+//     int N_h = 3;
 
-    double expected_u[] = {
-        0,0,0,0,0,
-        0,1,2,1,0,
-        0,2,4,2,0,
-        0,1,2,1,0,
-        0,0,0,0,0
-    };
+//     double expected_u[] = {
+//         0,0,0,0,0,
+//         0,1,2,1,0,
+//         0,2,4,2,0,
+//         0,1,2,1,0,
+//         0,0,0,0,0
+//     };
 
-    prolongation(u_2h,N_2h,u_h,N_h);
-    //vec_print(N_h,u_h,"u_h prolongation");
+//     prolongation(u_2h,N_2h,u_h,N_h);
+//     //vec_print(N_h,u_h,"u_h prolongation");
 
-    int fail = 0;
-    for (int i = 0;i<25;i++){
-        if (u_h[i]!=expected_u[i]){
-            fail = 1;
-        }
-    }
-    if (fail==0){
-        printf("TC1 successful\n");
-    }
-    else {
-        printf("TC1 failed\n");
-    }
-    printf("Test prolongation - END\n\n");
+//     int fail = 0;
+//     for (int i = 0;i<25;i++){
+//         if (u_h[i]!=expected_u[i]){
+//             fail = 1;
+//         }
+//     }
+//     if (fail==0){
+//         printf("TC1 successful\n");
+//     }
+//     else {
+//         printf("TC1 failed\n");
+//     }
+//     printf("Test prolongation - END\n\n");
     
-}
+// }
 
-void test_restriction(){
-    printf("Testing  restriction - START \n");
-    double expected_u[] = {
-        0,0,0,
-        0,2.5,0,
-        0,0,0   };
-    int N_2h=1;
+// void test_restriction(){
+//     printf("Testing  restriction - START \n");
+//     double expected_u[] = {
+//         0,0,0,
+//         0,2.5,0,
+//         0,0,0   };
+//     int N_2h=1;
 
-    double u_2h[9];
-    null_vec(u_2h,9);
+//     double u_2h[9];
+//     null_vec(u_2h,9);
 
-    int N_h = 3;
-    double u_h[] = {
-        0,0,0,0,0,
-        0,1,1,1,0,
-        0,1,4,1,0,
-        0,1,1,1,0,
-        0,0,0,0,0
-    };
+//     int N_h = 3;
+//     double u_h[] = {
+//         0,0,0,0,0,
+//         0,1,1,1,0,
+//         0,1,4,1,0,
+//         0,1,1,1,0,
+//         0,0,0,0,0
+//     };
 
-    restriction(u_h,N_h, u_2h,N_2h);
+//     restriction(u_h,N_h, u_2h,N_2h);
 
-    int fail = 0;
-    for (int i = 0;i<9;i++){
-        if (u_2h[i]!=expected_u[i]){
-            fail = 1;
-        }
-    }
-    if (fail==0){
-        printf("TC1 successful\n");
-    }
-    else {
-        printf("TC1 failed\n");
-    }
-    printf("Test restriction - END\n\n");
+//     int fail = 0;
+//     for (int i = 0;i<9;i++){
+//         if (u_2h[i]!=expected_u[i]){
+//             fail = 1;
+//         }
+//     }
+//     if (fail==0){
+//         printf("TC1 successful\n");
+//     }
+//     else {
+//         printf("TC1 failed\n");
+//     }
+//     printf("Test restriction - END\n\n");
     
-}
+// }
 
-void test_gaussian_elemination(){
-    printf("Testing gaussian elemination - START \n");
+// void test_gaussian_elemination(){
+//     printf("Testing gaussian elemination - START \n");
 
-    //TC1
-    double A[] = {
-        1,2,
-        3,4
-    };
-    double u[2];
-    double f[] = {5,5};
+//     //TC1
+//     double A[] = {
+//         1,2,
+//         3,4
+//     };
+//     double u[2];
+//     double f[] = {5,5};
 
-    gaussian_elimination(A,f,u,2);
-    print_1dim(2,u,"u gaussian TC1");  
+//     gaussian_elimination(A,f,u,2);
+//     print_1dim(2,u,"u gaussian TC1");  
 
-    double expected_u[]={-5,5};
-    if (u[0]!=expected_u[0] || u[1]!=expected_u[1]){
-        printf("TC1 failed\n");
-    }
-    else {
-        printf("TC1 successful\n");
-    }
-    printf("Testing gaussian elemination - END \n\n");
-}
+//     double expected_u[]={-5,5};
+//     if (u[0]!=expected_u[0] || u[1]!=expected_u[1]){
+//         printf("TC1 failed\n");
+//     }
+//     else {
+//         printf("TC1 successful\n");
+//     }
+//     printf("Testing gaussian elemination - END \n\n");
+// }
 
 void test_jacobi_smoothing(){
     printf("Testing jacobi smoothing - START \n");
 
     printf("TC1 - Convergences with only Jacobi\n");
-    int N = 49;
+    int N = 70;
     int vec_size = (N+2)*(N+2);
 
     double u[vec_size];
     null_vec(u,vec_size);
-    rand_vec(u,N);
+    rand_vec(u, N, 2);
 
     double b[vec_size];
     null_vec(b,vec_size);
@@ -152,27 +151,74 @@ void test_jacobi_smoothing(){
     double r[vec_size];
     null_vec(r,vec_size);
 
-    for (int i = 0; i<10000; i++){
-        smooth_jacobi(u,b,N,1);
+    for (int i = 0; i<5000; i++){
+        smooth_jacobi(u,b,N,1,2,0);
     }
+    // print_matrix(N+2, u);
 
     // calculate new residual
     mfMult(N, u, r);                //Au
     axpy(r, -1, r, b, vec_size);    //r= f-Au
 
     double err = norm(r,vec_size);
-    printf("Error in jacobi: %f\n",err);
+    printf("Error: %f\n",err);
 
 
     printf("Testing jacobi smoothing - END \n\n");
 }
 
+// void print_matrix(int N, double *matrix){
+//     for(int i = 0; i<N; i++){
+//         for(int j = 0; j<N; j++){
+//             printf("&f  ", matrix[i * N + j]);
+//         }
+//         printf("\n");
+//     }
+//     printf("\n");
+// }
+
+void test_richardson_smoothing(){
+    printf("Testing Richardson smoothing - START \n");
+
+    printf("TC1 - Convergences with only Richardson\n");
+    int N = 5;
+    int vec_size = (N+2)*(N+2);
+
+    double u[vec_size];
+    null_vec(u,vec_size);
+    rand_vec(u, N, 2);
+
+    double b[vec_size];
+    null_vec(b,vec_size);
+    init_b(b,N);
+    
+    double r[vec_size];
+    null_vec(r,vec_size);
+
+
+    for (int i = 0; i<20000; i++){
+        smooth_richardson(u,b,N,1,2,0);
+    }
+
+    print_matrix(N+2, u);
+
+    // calculate new residual
+    mfMult(N, u, r);                //Au
+    axpy(r, -1, r, b, vec_size);    //r= f-Au
+
+    double err = norm(r,vec_size);
+    printf("Error: %f\n",err);
+
+
+    printf("Testing Richardson smoothing - END \n\n");
+}
 
 int main(){
-    test_norm();
-    test_prolongation();
-    test_restriction();
-    test_gaussian_elemination();
+    // test_norm();
+    // test_prolongation();
+    // test_restriction();
+    // test_gaussian_elemination();
     test_jacobi_smoothing();
+    test_richardson_smoothing();
     return 0;
 }
