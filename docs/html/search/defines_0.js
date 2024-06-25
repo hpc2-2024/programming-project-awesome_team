97d37542cc35b2c8bbad3f165c985e21c9f78694
Jupyter01 <nujabes.feather.xor@gmail.com>
2024-06-25 20:32:17 +0200

    remove the docs folder

--- docs/html/search/defines_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['vcycle_0',['VCYCLE',['../w__cycle_8c.html#a341c0e66026202386ae2e42536216cff',1,'w_cycle.c']]]
-];
--- docs/html/search/files_9.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['w_5fcycle_2ec_0',['w_cycle.c',['../w__cycle_8c.html',1,'']]]
-];
